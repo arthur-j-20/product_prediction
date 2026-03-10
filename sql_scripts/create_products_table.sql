44de12d154b0ceff3d4b7928cb39f6785fc01dba
Santosh-J <santoshjada20@gmail.com>
2026-03-10 18:46:14 -0500

    added products table sql

---
+++ sql_scripts/create_products_table.sql
@@ -0,0 +1,9 @@
+create table insurance.products.insur_products(
+    product_id INT PRIMARY KEY,
+    cust_id INT,
+    product_auto BIT,
+    product_home BIT,
+    product_life BIT,
+    product_health BIT,
+    FOREIGN KEY(cust_id) REFERENCES insurance.products.customers(CustomerID)
+);
